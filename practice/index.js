2bfdb5a54bd461770f0c9b42e9563bcfdf454d73
Cherniugov Yevgenii <zhenek211@gmail.com>
2022-05-23 13:59:14 +0300

    Test commit

--- practice/index.js
+++ practice/index.js
@@ -611,13 +611,14 @@
 //   console.log('is child');
 // }
 
-const sumPositiveNumbers = (a, b) => {
-  if (typeof a !=='number' || typeof b !== 'number') {
-    return 'One of the arguments is not a number '
-  }
-  if (a <= 0 || b <= 0 ) {
-    return 'Numbers are not positive'
-  }
-}
+// const sumPositiveNumbers = (a, b) => {
+//   if (typeof a !=='number' || typeof b !== 'number') {
+//     return 'One of the arguments is not a number '
+//   }
+//   if (a <= 0 || b <= 0 ) {
+//     return 'Numbers are not positive'
+//   }
+// }
 
-console.log(sumPositiveNumbers(-2, 2))
+// console.log(sumPositiveNumbers(-2, 2))
+Hello
